2feac1d4a0fb944291e2a1c1ea6194b3584409d3
Leo-cloud_shell <interview@jetblue>
2025-04-08 17:40:10 +0000

    api stuff

--- data/recall_data.py
+++ data/recall_data.py
@@ -3,10 +3,22 @@ from dotenv import load_dotenv
 from typing import Any, Optional
 from datetime import datetime
 import requests
+from urllib.parse import urlencode
 import pyarrow as pa
 import pyarrow.parquet as pq
 
-base_url = f'https://api.nasa.gov/neo/rest/v1/neo/browse?'
+base_url = f"https://api.nasa.gov/neo/rest/v1/neo/browse"
+params = {"api_key": get_api_key(), "page": None, "size": None}
+
+
+def get_api_key(path: str) -> str:
+    load_dotenv(dotenv_path=path)
+    api_key = os.getenv("API_KEY")
+    if api_key:
+        return api_key
+
+    raise ValueError(f"API_KEY not found in {path}.env file.")
+
 
 schema = pa.schema(
     [
@@ -14,12 +26,12 @@ schema = pa.schema(
         pa.field("neo_reference_id", pa.int64(), nullable=False),
         pa.field("name", pa.string(), nullable=True),
         pa.field("name_limited", pa.string(), nullable=True),
-        pa.field("designation", pa.int64() , nullable=True),
+        pa.field("designation", pa.int64(), nullable=True),
         pa.field("nasa_jpl_url", pa.string(), nullable=True),
         pa.field("absolute_magnitude_h", pa.float64(), nullable=True),
         pa.field("is_potentially_hazardous_asteroid", pa.bool_(), nullable=True),
         pa.field("estimated_diameter_min", pa.float64(), nullable=True),
-        pa.field("estimated_diameter_max", pa.float64(), nullable=True)
+        pa.field("estimated_diameter_max", pa.float64(), nullable=True),
     ]
 )
 
@@ -33,7 +45,7 @@ def nested_get(d: dict[str, Any], keys: list[str], default: Any = None):
     return d
 
 
-def process_batch(obj: dict[str, Any], table_schema: pa.Schema)-> pa.Table:
+def process_batch(obj: dict[str, Any], table_schema: pa.Schema) -> pa.Table:
     data = {
         "id": [item.get("id") for item in obj],
         "neo_reference_id": [item.get("neo_reference_id") for item in obj],
@@ -57,7 +69,15 @@ def process_batch(obj: dict[str, Any], table_schema: pa.Schema)-> pa.Table:
     table = pa.Table.from_pydict(data, schema=table_schema)
     return table
 
-def store_batch(batch: pa.Table, path: str, batch_number: int)-> None:
-    pq.write_table(table, f"{path}/nasa_neo_data_{batch_number}.parquet", compression='snappy')
+
+def store_batch(batch: pa.Table, path: str, batch_number: int) -> None:
+    pq.write_table(
+        table, f"{path}/nasa_neo_data_{batch_number}.parquet", compression="snappy"
+    )
 
 
+def get_neo_data_batch(base_url: str, params: dict[str, Any], batch_size: int):
+
+    out = requests.get(f"{base_url}?{urlencode(params)}")
+    if out.ok:
+        return out.json()
