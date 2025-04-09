139e3150e7a7712cb3f79b5532c7df0623c3bf3f
Leo-cloud_shell <interview@jetblue>
2025-04-09 14:01:11 +0000

    refactor

--- data/data_processing.py
+++ data/data_processing.py
@@ -6,7 +6,7 @@ import pyarrow.parquet as pq
 from api_interface import nested_get
 
 
-schema = pa.schema(
+main_schema = pa.schema(
     [
         pa.field("id", pa.int64(), nullable=False),
         pa.field("neo_reference_id", pa.int64(), nullable=False),
@@ -24,26 +24,44 @@ schema = pa.schema(
     ]
 )
 
+close_approach_schema = pa.schema(
+    [
+        pa.field("id", pa.int64(), nullable=False),
+        pa.field("neo_reference_id", pa.int64(), nullable=False),
+        pa.field("name", pa.string(), nullable=True),
+        pa.field("name_limited", pa.string(), nullable=True),
+        pa.field("designation", pa.int64(), nullable=True),
+        pa.field("nasa_jpl_url", pa.string(), nullable=True),
+        pa.field("absolute_magnitude_h", pa.float64(), nullable=True),
+        pa.field("is_potentially_hazardous_asteroid", pa.bool_(), nullable=True),
+        pa.field("estimated_diameter_min", pa.float64(), nullable=True),
+        pa.field("first_observation_date", pa.timestamp("s"), nullable=False),
+        pa.field("last_observation_date", pa.timestamp("s"), nullable=False),
+        pa.field("observations_used", pa.int64(), nullable=True),
+        pa.field("orbital_period", pa.float64(), nullable=True),
+    ]
+)
+
+def to_int(value: Any)-> Optional[int]:
+    if value is None:
+        return None
+    try:
+        return int(value)
+    except (ValueError, TypeError):
+        print(f"Warning: Could not convert '{value}' to int. Setting to None.")
+        return None
+
+def to_float(value: Any)-> Optional[float]:
+    if value is None:
+        return None
+    try:
+        return float(value)
+    except (ValueError, TypeError):
+        print(f"Warning: Could not convert '{value}' to int. Setting to None.")
+        return None
+
 
 def process_batch(obj: dict[str, Any], table_schema: pa.Schema) -> pa.Table:
-    def to_int(value: Any):
-        if value is None:
-            return None
-        try:
-            return int(value)
-        except (ValueError, TypeError):
-            print(f"Warning: Could not convert '{value}' to int. Setting to None.")
-            return None
-
-    def to_float(value: Any):
-        if value is None:
-            return None
-        try:
-            return float(value)
-        except (ValueError, TypeError):
-            print(f"Warning: Could not convert '{value}' to int. Setting to None.")
-            return None
-
     data = {
         "id": [to_int(item.get("id")) for item in obj],
         "neo_reference_id": [to_int(item.get("neo_reference_id")) for item in obj],
@@ -100,3 +118,6 @@ def store_batch(batch: pa.Table, destination_path: str, batch_number: int) -> No
         os.path.join(destination_path, f"nasa_neo_data_{batch_number}.parquet"),
         compression="snappy",
     )
+
+def process_close_approaches(obj: dict[str, Any], table_schema: pa.Schema) -> pa.Table:
+    pass
