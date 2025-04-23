b20ee89462328186727a31335f38d52d38816385
Leo-cloud_shell <interview@jetblue>
2025-04-23 14:46:00 +0000

    safer

--- data_processing.py
+++ data_processing.py
@@ -182,7 +182,7 @@ def process_batch(
         ],
     }
     table = pa.Table.from_pydict(data, schema=table_schema)
-    return table, sum([x for x in data["very_close_approaches"] if x is not None])
+    return table, sum([x if x is not None else 0 for x in data["very_close_approaches"]])
 
 
 def store_batch(
